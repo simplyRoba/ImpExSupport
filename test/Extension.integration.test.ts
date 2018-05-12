386aa6a4d65fbf795a7588b61f4ac15e8688aa08
Axel <git@simplyroba.de>
2018-05-12 22:05:56 +0200

    used another resoure

--- test/Extension.integration.test.ts
+++ test/Extension.integration.test.ts
@@ -1,12 +1,12 @@
 
 import { Extension, workspace, window, extensions } from "vscode";
 import { expect } from "chai";
-import { TestColumnHiglighterImpex } from "./Resources";
+import { Sample1Impex } from "./Resources";
 
 suite("Extension Integration Test", () => {
 
     test("should get activated by opening an impex file", () => {
-        workspace.openTextDocument(TestColumnHiglighterImpex).then((document) => {
+        workspace.openTextDocument(Sample1Impex).then((document) => {
 
             window.showTextDocument(document).then((editor) => {
                 let extension: Extension<any> = extensions.getExtension("simplyRoba.impex-support");

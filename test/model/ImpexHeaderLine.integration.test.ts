4f78f7549953e8907541d132dd4bf4494be32324
Axel <git@simplyroba.de>
2018-05-13 09:01:23 +0200

    test for header line

--- test/model/ImpexHeaderLine.integration.test.ts
+++ test/model/ImpexHeaderLine.integration.test.ts
@@ -23,6 +23,41 @@ suite("ImpexHeaderLine Integration Test", () => {
         expect(columns2.length).to.be.equals(6);
     });
 
+    test("should be the correct line number", () => {
+        let lineNumber: number = 12;
+        let line: ImpexHeaderLine = new ImpexHeaderLine(createTextLine(lineNumber, "INSERT Item;field1;field2;"));
+
+        let range: Range = line.rangeForColumnAtIndex(2);
+
+        expect(range.start.line).to.be.equals(lineNumber);
+        expect(range.end.line).to.be.equals(lineNumber);
+    });
+
+    test("should return correct range for index", () => {
+        let line: ImpexHeaderLine = new ImpexHeaderLine(createTextLine(44, "REMOVE Item;field1;field2;"));
+
+        let range: Range = line.rangeForColumnAtIndex(1);
+
+        expect(range.start.character).to.be.equals(12);
+        expect(range.end.character).to.be.equals(18);
+    });
+
+    test("should return correct index for position", () => {
+        let line: ImpexHeaderLine = new ImpexHeaderLine(createTextLine(34, "UPDATE Item;field1;field2;"));
+
+        let index: number = line.columnIndexOfPostion(14);
+
+        expect(index).to.be.equals(1);
+    });
+
+    test("should return correct range for position", () => {
+        let line: ImpexHeaderLine = new ImpexHeaderLine(createTextLine(12, "UPDATE Item;field[unique=true];field"));
+
+        let range: Range = line.rangeForColumnAtPosition(18);
+
+        expect(range.start.character).to.be.equals(12);
+        expect(range.end.character).to.be.equals(30);
+    });
 });
 
 function createTextLine(lineNumber: number, text: string): TextLine {
